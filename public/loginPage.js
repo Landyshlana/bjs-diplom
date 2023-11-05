d24f5851e007aa5b11961519486bed27c23c2683
Landysh Sagitova <landysh.sagitova@mail.ru>
2023-11-05 21:37:17 +0300

    дипломная работа выполнена частично

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,15 +1,20 @@
 "use strict";
 const userForm = new UserForm();
 userForm.loginFormCallback = (data) => {
-        ApiConnector.login(data, (response) => {
-                if (response){location.reload();} else {
-              setLoginErrorMessage();
-    }
-    });
+	ApiConnector.login(data, (response) => {
+		if (response) {
+			location.reload();
+		} else {
+			setLoginErrorMessage();
+		}
+	});
 }
 userForm.registerFormCallback = (data) => {
-    ApiConnector.register(data, (response) => {
-        if (response){location.reload();} else {
-            setRegisterErrorMessage();}
-    });
+	ApiConnector.register(data, (response) => {
+		if (response) {
+			location.reload();
+		} else {
+			setRegisterErrorMessage();
+		}
+	});
 }
